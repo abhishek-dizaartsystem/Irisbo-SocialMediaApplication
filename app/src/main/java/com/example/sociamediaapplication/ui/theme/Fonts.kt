014ed83cb3e0f82adb73b0a671edbd3660e52b3a
abhi-patwal-02 <abhishek.patwal.2004@gmail.com>
2026-03-14 17:48:17 +0530

    optimized content of ui/theme

--- app/src/main/java/com/example/sociamediaapplication/ui/theme/Fonts.kt
+++ app/src/main/java/com/example/sociamediaapplication/ui/theme/Fonts.kt
@@ -3,7 +3,6 @@ package com.example.sociamediaapplication.ui.theme
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import com.example.sociamediaapplication.R
-import kotlin.random.Random
 
 val BabyPlums = FontFamily(
     Font(R.font.babyplums)
