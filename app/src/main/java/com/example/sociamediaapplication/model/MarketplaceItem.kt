f095d2a33eab9daf085033ec66fefd19e242c5f9
abhi-patwal-02 <abhishek.patwal.2004@gmail.com>
2026-03-14 17:47:17 +0530

    optimized content of model/response

--- app/src/main/java/com/example/sociamediaapplication/model/MarketplaceItem.kt
+++ app/src/main/java/com/example/sociamediaapplication/model/MarketplaceItem.kt
@@ -1,7 +1,5 @@
 package com.example.sociamediaapplication.model
 
-import androidx.compose.ui.graphics.painter.Painter
-
 data class MarketplaceItem(
     val productId: String,
     val painter: Int,
--- app/src/main/java/com/example/sociamediaapplication/model/VideoAnalyticsCardModel.kt
+++ app/src/main/java/com/example/sociamediaapplication/model/VideoAnalyticsCardModel.kt
@@ -1,8 +1,6 @@
 package com.example.sociamediaapplication.model
 
-import androidx.annotation.DrawableRes
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
 
 data class VideoAnalyticsCardModel(
     val icon: Int,
