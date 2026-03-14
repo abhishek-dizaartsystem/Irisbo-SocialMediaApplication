d7bab35a58f06b2f5057ab175656c15659cea0d4
abhi-patwal-02 <abhishek.patwal.2004@gmail.com>
2026-03-14 17:45:41 +0530

    optimized content of model/editor

--- app/src/main/java/com/example/sociamediaapplication/model/editor/TextLayer.kt
+++ app/src/main/java/com/example/sociamediaapplication/model/editor/TextLayer.kt
@@ -4,7 +4,6 @@ package com.example.sociamediaapplication.model.editor
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.IntSize
 
